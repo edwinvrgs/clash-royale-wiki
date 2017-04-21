62a7e3b21c4ab6e7257616769e8d46735546f832
nelxaz <nelsonx.zambrano@gmail.com>
2017-04-20 21:58:44 -0400

    Actualiza dependencias RRv3 a RRv4

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ var BUILD_DIR = path.resolve(__dirname, './public');
 var APP_DIR = path.resolve(__dirname, './app');
 
 module.exports = {
-  entry: APP_DIR + '/index.jsx',
+  entry: APP_DIR + '/index.js',
   output: {
     path: BUILD_DIR,
     filename: 'bundle.js'
