08f0f81dc0ae529b340739fa396e4cf1ed98a7d5
nelxaz <nelsonx.zambrano@gmail.com>
2017-04-29 21:32:13 -0400

    Añade imagenes

--- app/components/Carta.js
+++ app/components/Carta.js
@@ -3,10 +3,12 @@ import React, { Component } from 'react';
 class Carta extends Component {
     
     render() {
-        const {name, elixirCost, type, rarity,description} = this.props;
+        const {name, elixirCost, type, rarity,description, idName} = this.props;
+        const base_url = "http://www.clashapi.xyz/images/cards/"
         return (
             <div>
                 <span>
+                    <img src={base_url+idName+".png"} alt=""/>
                     {name}-
                     {elixirCost}-
                     {type}-
